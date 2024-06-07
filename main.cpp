a5a5668be8cb8133701ff822fc4ec26ae3cb9686
Helena Jońca <helenajonca@onet.pl>
2024-06-07 18:13:22 +0200

    Dodanie plikow

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <wx/wx.h>
+#include <wx/dcbuffer.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include "Maze3D.h"
+
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit() 
+    {
+        Maze3D* maze = new Maze3D("Labirynt 3D", "labirynt.txt");
+        maze->Show(true);
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);
