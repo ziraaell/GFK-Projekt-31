27942bd67b3d50ff5883f3218c609d9b87982e8f
Helena Jońca <helenajonca@onet.pl>
2024-06-08 12:26:28 +0200

    Dodanie plikow i folderow

---
+++ include/Maze3D.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <wx/wx.h>
+#include "Player.h"
+
+enum class FIELD { PLAYER, WALL, FLOOR, EXIT };
+
+class Maze3D : public wxFrame 
+{
+public:
+    Maze3D(const std::string& title, const std::string& filename);
+    void LoadMapFromFile(const std::string& filename);
+    void OnPaint(wxPaintEvent& event);
+    void Render3D(wxDC& dc);
+    //void RenderMap(wxDC& dc);
+    void OnKeyDown(wxKeyEvent& event);
+    void OnKeyUp(wxKeyEvent& event);
+    void HandleMovement();
+    void DisplayTime();
+    ~Maze3D();
+
+private:
+    bool keys[4] = {false, false, false, false};
+    bool shiftPressed = false;
+    std::vector <std::vector <FIELD>> Fields;
+    int width, height;
+    Player player;
+    time_t startTime;
+    bool gameEnded = false;
+};
---
+++ include/Player.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct Player {
+    float pos_x, pos_y;
+    float angle;
+    float speed;
+    float turnSpeed;
+    bool isRunning;
+};
