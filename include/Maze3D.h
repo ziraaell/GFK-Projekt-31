c1f56fbb4f174ed285363cd0caef23135c91137b
Julia Przezdzik <113039967+juleczzzka@users.noreply.github.com>
2024-06-13 11:47:02 +0200

    Update Maze3D.h

--- include/Maze3D.h
+++ include/Maze3D.h
@@ -2,8 +2,19 @@
 #include <wx/wx.h>
 #include "Player.h"
 
+/**
+ * @enum FIELD
+ * @brief Typ wyliczeniowy reprezentujący rodzaje pól w labiryncie.
+ */
 enum class FIELD { PLAYER, WALL, FLOOR, EXIT };
 
+/**
+ * @class Maze3D
+ * @brief Klasa głównego okna aplikacji, obsługująca logikę i wyświetlanie 3D labiryntu.
+ *
+ * Maze3D dziedziczy po wxFrame i zarządza wszystkimi głównymi aspektami interakcji z użytkownikiem oraz
+ * wizualizacją i obsługą logiki gry labirynt.
+ */
 class Maze3D : public wxFrame 
 {
 public:
@@ -19,17 +30,17 @@ public:
     ~Maze3D();
 
 private:
-    bool keys[4] = {false, false, false, false};
-    bool shiftPressed = false;
-    std::vector <std::vector <FIELD>> Fields;
-    int width, height;
-    Player player;
-    time_t startTime;
-    bool gameEnded = false;
-    bool FPP = true;
+    bool keys[4] = {false, false, false, false}; ///< Statusy klawiszy: UP, DOWN, LEFT, RIGHT
+    bool shiftPressed = false;                  ///< Status klawisza SHIFT
+    std::vector <std::vector <FIELD>> Fields;   ///< Struktura przechowująca typy pól labiryntu
+    int width, height;                          ///< Wymiary labiryntu
+    Player player;                              ///< Obiekt gracza
+    time_t startTime;                           ///< Czas rozpoczęcia gry
+    bool gameEnded = false;                     ///< Flaga końca gry
+    bool FPP = true;                            ///< Flaga widoku pierwszoosobowego (First Person Perspective)
 
     enum {
-        ID_1 = 1001,
-        ID_2 = 1002,
+        ID_1 = 1001, ///< ID dla menu zmiany na widok pierwszoosobowy
+        ID_2 = 1002, ///< ID dla menu zmiany na widok z góry
     };
 };
