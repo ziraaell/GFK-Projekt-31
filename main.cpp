301a4b7ca6b745995d8a917858f5cf1acac99391
Julia Przezdzik <113039967+juleczzzka@users.noreply.github.com>
2024-06-13 11:48:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/**
+ * @file MyApp.cpp
+ * @brief Plik zawiera klasę MyApp, która jest punktem wejścia aplikacji wxWidgets.
+ */
+
 #include <wx/wx.h>
 #include <wx/dcbuffer.h>
 #include <fstream>
@@ -7,8 +12,21 @@
 #include <cmath>
 #include "Maze3D.h"
 
+
+/**
+ * @class MyApp
+ * @brief Klasa aplikacji, dziedzicząca po wxApp, odpowiedzialna za inicjalizację i uruchomienie głównego okna aplikacji.
+ * 
+ * MyApp odpowiada za utworzenie i wyświetlenie głównego okna aplikacji oraz załadowanie potrzebnych zasobów.
+ */
 class MyApp : public wxApp {
 public:
+/**
+     * @brief Inicjalizuje aplikację.
+     * 
+     * Tworzy i wyświetla główne okno aplikacji. Ładuje główny widok z pliku labirynt.txt.
+     * @return true jeśli inicjalizacja przebiegła pomyślnie, false w przeciwnym razie.
+     */
     virtual bool OnInit() 
     {
         Maze3D* maze = new Maze3D("Labirynt 3D", "labirynt.txt");
@@ -17,4 +35,9 @@ public:
     }
 };
 
+/**
+ * @brief Makro odpowiedzialne za implementację głównego punktu wejścia aplikacji wxWidgets.
+ * 
+ * wxIMPLEMENT_APP(MyApp) implementuje główną pętlę programu oraz inicjalizuje aplikację wxWidgets.
+ */
 wxIMPLEMENT_APP(MyApp);
