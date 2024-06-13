4659c5a9aa1c61dc57970b2970c8c74595e9f8f0
Julia Przezdzik <113039967+juleczzzka@users.noreply.github.com>
2024-06-13 11:47:37 +0200

    Update Player.h

--- include/Player.h
+++ include/Player.h
@@ -1,9 +1,16 @@
 #pragma once
 
+/**
+ * @struct Player
+ * @brief Struktura reprezentująca gracza w grze.
+ * 
+ * Struktura Player przechowuje wszystkie kluczowe informacje dotyczące pozycji, orientacji, prędkości i stanu biegu gracza w symulacji lub grze.
+ */
 struct Player {
-    float pos_x, pos_y;
-    float angle;
-    float speed;
-    float turnSpeed;
-    bool isRunning;
+    float pos_x; ///< Pozycja gracza na osi X.
+    float pos_y; ///< Pozycja gracza na osi Y.
+    float angle; ///< Kąt orientacji gracza w radianach.
+    float speed; ///< Aktualna prędkość liniowa gracza.
+    float turnSpeed; ///< Prędkość obrotu gracza (zmiana kąta na jednostkę czasu).
+    bool isRunning; ///< Flaga sprawdzajaca, czy gracz biegnie
 };
